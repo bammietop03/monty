4362983e255ad728df214fb28368e40da7f7f160
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-20 05:10:58 -0700

    Implemented the add opcode.

--- stack_ops2.c
+++ stack_ops2.c
@@ -7,17 +7,16 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *next_el, *top;
+	stack_t *next_el, *top = *stack;
 	int sum = 0;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	next_el = (*stack)->next;
-	top = *stack;
 
 	sum = top->n + next_el->n;
 	next_el->n = sum;
