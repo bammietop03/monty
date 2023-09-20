a50500c1144ab5cd0d35c93cafc8decae2bff283
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-09-20 04:21:00 -0700

    Updated push and pall opcodes to pass all checks.

--- stack_ops.c
+++ stack_ops.c
@@ -47,14 +47,15 @@ void push(stack_t **stack, unsigned int line_number)
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
-	/*Suppress unused parameter warning*/
-	(void)line_number;
 
 	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 	}
+
+	/*Suppress unused parameter warning*/
+	(void)line_number;
 }
 
 /**
