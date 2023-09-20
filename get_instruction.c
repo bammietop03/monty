c0e8aec8728a43c9fda5b5021805fb992e766d2d
bammietop03 <bammietop@gmail.com>
2023-09-20 15:45:57 +0100

    Implement the mod opcode.

--- get_instruction.c
+++ get_instruction.c
@@ -80,6 +80,10 @@ int process_file(const char *file_name)
 		line_number++;
 		opcode = strtok(line, " \n");
 
+		if (opcode == NULL || strcmp(opcode, "\n") == 0 ||
+				strcmp(opcode, "#") == 0)
+			continue;
+
 		if (opcode)
 		{
 			if (!match_instruction(opcode, &stack, line_number))
